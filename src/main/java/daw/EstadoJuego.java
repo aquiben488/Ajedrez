f34468cb72efa17b50a3d06cf0e5608d61638a05
Alejandro Quirós <aquiben488@g.educaand.es>
2024-12-28 22:08:47 +0100

    Estado de juego terminado

--- src/main/java/daw/EstadoJuego.java
+++ src/main/java/daw/EstadoJuego.java
@@ -8,30 +8,45 @@ public enum EstadoJuego {
     // Mensaje del turno
     // Si hay jaque mate o rey ahogado (fin de juego) 
 
-    TURNO_BLANCAS(true,false,"Turno de las blancas"),
-    TURNO_NEGRAS(false,false,"Turno de las negras"),
-    TURNO_BLANCAS_FIN(true,true,"Jaque mate, las blancas han ganado"),
-    TURNO_NEGRAS_FIN(false,true,"Jaque mate, las negras han ganado"),
-    TURNO_FIN_REY_AHOGADO(null,true,"Rey ahogado, hay empate");
+    TURNO_BLANCAS(true,false,false,"Turno de las blancas"),
+    TURNO_NEGRAS(false,false,false,"Turno de las negras"),
+
+    JAQUE_BLANCAS(true,true,false,"Turno de las blancas, hay jaque"),
+    JAQUE_NEGRAS(false,true,false,"Turno de las negras, hay jaque"),
+
+    JAQUE_MATE_BLANCAS(true,true,true,"Jaque mate, las negras han ganado"),
+    JAQUE_MATE_NEGRAS(false,true,true,"Jaque mate, las blancas han ganado"),
+
+    REY_AHOGADO(null,true,true,"Rey ahogado, hay empate");
 
     private final Boolean turno;
+    private final boolean jaque;
     private final boolean fin;
     private final String mensaje;
 
-    private EstadoJuego(Boolean turno, boolean fin, String mensaje) {
+    private EstadoJuego(Boolean turno,boolean jaque, boolean fin, String mensaje) {
         this.turno = turno;
+        this.jaque = jaque;
         this.fin = fin;
         this.mensaje = mensaje;
     }
     
-    
+    public boolean puedeJugar(boolean colorPieza) {
+        return this.turno == colorPieza;
+    }
 
-
-
-
-
-
-
+    public EstadoJuego siguienteTurno(boolean jaque, boolean fin, boolean reyAhogado) {
+        if (reyAhogado) {
+            return REY_AHOGADO;
+        }
+        if (fin) {
+            return this.turno ? JAQUE_MATE_NEGRAS : JAQUE_MATE_BLANCAS;
+        }
+        if (jaque) {
+            return this.turno ? JAQUE_NEGRAS : JAQUE_BLANCAS;
+        }
+        return this.turno ? TURNO_NEGRAS : TURNO_BLANCAS;
+    }
 
     public String getMensaje() {
         return this.mensaje;
@@ -45,5 +60,10 @@ public enum EstadoJuego {
         return this.fin;
     }
 
+    public boolean esJaque() {
+        return this.jaque;
+    }   
+
    
+
 }
